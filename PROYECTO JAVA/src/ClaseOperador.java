2c2ac9b6f88960d322156640ee90428bf4a0b5aa
Jhon <Jhonsan806@gmail.com>
2025-03-01 17:18:01 -0500

    Ejercicios

---
+++ PROYECTO JAVA/src/ClaseOperador.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ClaseOperador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el primer numero: ");
+        int numero1 = scanner.nextInt();
+        System.out.print("Ingrese el segundo numero: ");
+        int numero2 = scanner.nextInt();
+        System.out.print("ingrese el tercer numero: ");
+        int numero3 = scanner.nextInt();
+
+        boolean resultado= (numero1>numero2) & (numero1<numero3);
+        System.out.println("¿¿El primer número es mayor que el segundo y menor que el tercero?" + resultado);
+        
+        }
+}
